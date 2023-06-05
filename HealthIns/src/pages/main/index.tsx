87837c07e0a99c2b89f5424a8ef7aba98d433cb9
robin <nowrobin3@gmail.com>
2023-06-06 00:07:14 +0900

    feat : install 버튼 추가

--- HealthIns/src/pages/main/index.tsx
+++ HealthIns/src/pages/main/index.tsx
@@ -1,9 +1,23 @@
 import styles from "./styles.module.scss";
 
 export default function Mainpage() {
+  let deferredPrompt: any;
+  window.addEventListener("beforeinstallprompt", (event) => {
+    event.preventDefault();
+    deferredPrompt = event;
+  });
+  const installApp = () => {
+    if (!deferredPrompt) {
+      alert("이미 앱이 설치되어 있거나 앱을 설치할 수 없는 환경입니다");
+      return;
+    }
+
+    deferredPrompt.prompt();
+  };
   return (
     <>
       <div className={styles.main}>Hi</div>
+      <button onClick={installApp}>install</button>
     </>
   );
 }
